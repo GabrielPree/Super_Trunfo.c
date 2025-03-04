34ac879a2c0d33c56a20727eb99318b5007d64d2
Gabriel <gabriellopesp@hotmail.com>
2025-03-04 15:41:56 +0000

    Adicionei o calculo de densidade populacional e Pib per capita da carta 01

--- super_trunfo.c
+++ super_trunfo.c
@@ -3,12 +3,11 @@
 int main(){
  //Carta 01
     char nome[50], estado[50], Codigo[4];
-    int populacao;
-    float area;
-    double pib;
-    int ponto;
+    int populacao, ponto;
+    float area, densidade;
+    double pib, pibpercapita;
 
-    //Carta 01 coleta de informações 
+//Carta 01 coleta de informações 
     printf("** Super Trunfo Paises **\n");
     printf("Digite as Informações da Carta 01\n");
     printf("Digite o nome do estado: ");
@@ -25,14 +24,21 @@ int main(){
     scanf("%lf", &pib);
     printf("Digite o numero de pontos turisticos da cidade: ");
     scanf("%d", &ponto);
+
+//calculos da carta 01
+    densidade = populacao/area;
+    pibpercapita = (float)pib / populacao; // usei casting para garantir a precisão e evitar perda de dados
+
 //Carta 01 visualização de informações
     printf("\n\n** Informações da Carta 01 **\n");
     printf("Nome do Estado: %s\n", estado);
     printf("Codigo: %s\n", Codigo);
     printf("Nome da Cidade: %s\n", nome);
     printf("População: %d\n", populacao);
-    printf("Area: %.2f km²\n", area);
+    printf("Area: %.2f\n", area);
     printf("PIB: %.2lf\n", pib);
     printf("Numero de pontos turisticos: %d\n", ponto);
+    printf("Densidade: %.2f\n", densidade);
+    printf("PIB percapta: %.2lf\n", pibpercapita);
     return 0;
 }
