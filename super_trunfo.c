4788772e993f6ebebe8a7cb995f22639e3fe7319
Gabriel <gabriellopesp@hotmail.com>
2025-03-05 14:26:35 +0000

    Adicionei a coleta, calculo e exibição de dados da carta 02

--- super_trunfo.c
+++ super_trunfo.c
@@ -6,6 +6,11 @@ int main(){
     int populacao, ponto;
     float area, densidade;
     double pib, pibpercapita;
+//carta 02
+    char nome2[50], estado2[50], Codigo2[4];
+    int populacao2, ponto2;
+    float area2, densidade2;
+    double pib2, pibpercapita2; 
 
 //Carta 01 coleta de informações 
     printf("** Super Trunfo Paises **\n");
@@ -25,12 +30,31 @@ int main(){
     printf("Digite o numero de pontos turisticos da cidade: ");
     scanf("%d", &ponto);
 
-//calculos da carta 01
+//carta 02 coleta de informações
+    printf("\nDigite as Informações da Carta 02\n");
+    printf("Digite o nome do estado: ");
+    scanf(" %[^\n]", estado2);
+    printf("Digite o codigo do estado: ");
+    scanf(" %[^\n]", Codigo2);
+    printf("Digite o nome da cidade: ");
+    scanf(" %[^\n]", nome2);
+    printf("Digite a população da cidade: ");
+    scanf("%d", &populacao2);
+    printf("Digite a area da cidade: ");
+    scanf("%f", &area2);
+    printf("Digite o PIB da cidade: ");
+    scanf("%lf", &pib2);
+    printf("Digite o numero de pontos turisticos da cidade: ");
+    scanf("%d", &ponto2);
+
+//calculos da carta 01 e 02
     densidade = populacao/area;
     pibpercapita = (float)pib / populacao; // usei casting para garantir a precisão e evitar perda de dados
+    densidade2 = populacao2/area2;
+    pibpercapita2 = (float)pib2 / populacao2;
 
 //Carta 01 visualização de informações
-    printf("\n\n** Informações da Carta 01 **\n");
+    printf("\n** Informações da Carta 01 **\n");
     printf("Nome do Estado: %s\n", estado);
     printf("Codigo: %s\n", Codigo);
     printf("Nome da Cidade: %s\n", nome);
@@ -40,5 +64,17 @@ int main(){
     printf("Numero de pontos turisticos: %d\n", ponto);
     printf("Densidade: %.2f\n", densidade);
     printf("PIB percapta: %.2lf\n", pibpercapita);
+
+//carta 02 visualização de informações
+    printf("\n** Informações da Carta 02 **\n");
+    printf("Nome do Estado: %s\n", estado2);
+    printf("Codigo: %s\n", Codigo2);
+    printf("Nome da Cidade: %s\n", nome2);
+    printf("População: %d\n", populacao2);
+    printf("Area: %.2f\n", area2);
+    printf("PIB: %.2lf\n", pib2);
+    printf("Numero de pontos turisticos: %d\n", ponto2);
+    printf("Densidade: %.2f\n", densidade2);
+    printf("PIB percapta: %.2lf\n", pibpercapita2);
     return 0;
 }
