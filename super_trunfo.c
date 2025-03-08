8cb9555f904e10ed389f2b954a9bd62f7924d358
Gabriel <gabriellopesp@hotmail.com>
2025-03-08 22:19:23 +0000

    Adicionado o calculo do super poder e população como unsigned int

--- super_trunfo.c
+++ super_trunfo.c
@@ -3,14 +3,16 @@
 //Declaração de variaveis
  //variaveis Carta 01
     char nome[50], estado[50], Codigo[4];
-    int populacao, ponto;
+    unsigned int populacao;
+    int ponto;
     float area, densidade;
-    double pib, pibpercapita;
+    double pib, pibpercapita, superpoder;
 //variaveis carta 02
     char nome2[50], estado2[50], Codigo2[4];
-    int populacao2, ponto2;
+    unsigned int populacao2;
+    int ponto2;
     float area2, densidade2;
-    double pib2, pibpercapita2; 
+    double pib2, pibpercapita2, superpoder2; 
 
 void entradaDados() {
 //Carta 01 coleta de informações 
@@ -23,7 +25,7 @@ void entradaDados() {
     printf("Digite o nome da cidade: ");
     scanf(" %[^\n]", nome);//usei %[^\n] para que fosse possivel ler espaço no nome da cidade
     printf("Digite a população da cidade: ");
-    scanf("%d", &populacao);
+    scanf("%u", &populacao);
     printf("Digite a area da cidade: ");
     scanf("%f", &area);
     printf("Digite o PIB da cidade: ");
@@ -40,7 +42,7 @@ void entradaDados() {
     printf("Digite o nome da cidade: ");
     scanf(" %[^\n]", nome2);
     printf("Digite a população da cidade: ");
-    scanf("%d", &populacao2);
+    scanf("%u", &populacao2);
     printf("Digite a area da cidade: ");
     scanf("%f", &area2);
     printf("Digite o PIB da cidade: ");
@@ -49,12 +51,16 @@ void entradaDados() {
     scanf("%d", &ponto2);
 }
 
-float calculos() {
+void calculos() {
 //calculos da carta 01 e 02
-    densidade = populacao/area;
+    //carta 01
+    densidade = (float)populacao/area;
     pibpercapita = (float)pib / populacao; // usei casting para garantir a precisão e evitar perda de dados
-    densidade2 = populacao2/area2;
+    superpoder = (populacao + ponto + pib + area + pibpercapita) / densidade;
+    //carta 02
+    densidade2 = (float)populacao2/area2;
     pibpercapita2 = (float)pib2 / populacao2;
+    superpoder2 = (populacao2 + ponto2 + pib2 + area2 + pibpercapita2) / densidade2;
 }
 
 void exibirDados() {
@@ -63,24 +69,26 @@ void exibirDados() {
     printf("Nome do Estado: %s\n", estado);
     printf("Codigo: %s\n", Codigo);
     printf("Nome da Cidade: %s\n", nome);
-    printf("População: %d\n", populacao);
+    printf("População: %u\n", populacao);
     printf("Area: %.2f\n", area);
     printf("PIB: %.2lf\n", pib);
     printf("Numero de pontos turisticos: %d\n", ponto);
     printf("Densidade: %.2f\n", densidade);
     printf("PIB percapta: %.2lf\n", pibpercapita);
+    printf("Super Poder: %.2f\n", superpoder);
 
 //carta 02 visualização de informações
     printf("\n** Informações da Carta 02 **\n");
     printf("Nome do Estado: %s\n", estado2);
     printf("Codigo: %s\n", Codigo2);
     printf("Nome da Cidade: %s\n", nome2);
-    printf("População: %d\n", populacao2);
+    printf("População: %u\n", populacao2);
     printf("Area: %.2f\n", area2);
     printf("PIB: %.2lf\n", pib2);
     printf("Numero de pontos turisticos: %d\n", ponto2);
     printf("Densidade: %.2f\n", densidade2);
     printf("PIB percapta: %.2lf\n", pibpercapita2);
+    printf("Super Poder: %.2f\n", superpoder2);
 }
 int main() {
     entradaDados();
